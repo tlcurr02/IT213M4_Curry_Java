30e41ec3a76479d09f100b27b894c2e6a6feb874
Tamarra Curry <34217856+tlcurr02@users.noreply.github.com>
2023-12-03 21:16:28 -0500

    Update InitArraytwo.java

--- InitArraytwo.java
+++ InitArraytwo.java
@@ -10,12 +10,12 @@ public class InitArraytwo
 
       int array [] = { 32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
 
-      System.out.printf("%s%8s\n", "Index", Value"); // makes the column headings
+      System.out.printf("%s%8s\n", "Index", "Value"); // makes the column headings
 
       // output each array element's value
 
       for (int counter = 0;  counter < array.length; counter++ )
-        System.out.printf ( "%5d%8d/", counter array [ counter ] );
+        System.out.printf ( "%5d%8d/", counter, array [ counter ] );
 
 
       
